19348399342cc58c174c748e94f271617985592e
taminhluan <ngoalongtb001@gmail.com>
2020-02-04 13:25:44 +0700

    overwrite original_in_place

--- app.py
+++ app.py
@@ -19,6 +19,6 @@ for parent_dir in parent_dirs:
                     os.makedirs(result_dir)
                 os.system(f'magick {parent_dir}/{dir}/{image} -crop 50%x100% +repage {split_dir}/{image}_%02d.jpg')
                 os.system(f'convert +append {split_dir}/{image}_01.jpg {split_dir}/{image}_00.jpg {result_dir}/{image}')
-                os.system(f'exiftool -TagsFromFile {parent_dir}/{dir}/{image} {result_dir}/{image}')
+                os.system(f'exiftool -TagsFromFile -overwrite_original_in_place {parent_dir}/{dir}/{image} {result_dir}/{image} ')
 
 
